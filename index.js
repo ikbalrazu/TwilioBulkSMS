b4db984d3135ec96e1affb6733b2d446b6d892f2
Md Ikbal Razu <37942730+ikbalrazu@users.noreply.github.com>
2022-02-20 21:02:27 +0600

    twilio integration is not completed yet

--- index.js
+++ index.js
@@ -3,15 +3,42 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+const accountSid = 'ACadc3ffb43cef495abeafebce2609c137'; // Your Account SID from www.twilio.com/console
+const authToken = '1069b3764bb73164952c4effb0259a96'; // Your Auth Token from www.twilio.com/console
+
+const twilio = require('twilio');
+const client = new twilio(accountSid, authToken);
+
+function sendSMS() {
+    console.log("send sms method");
+    client.messages
+        .create({
+            body: 'Hello from Node by iqbal',
+            to: '+8801622869685', // Text this number
+            from: '+44 7480 609895', // From a valid Twilio number
+        })
+        .then((message) => console.log(message.sid));
+        
+
+    console.log("this is the function end");
+
+}
+
 app.get("/",(req,res)=>{
-    res.send("Hello world");
+    res.send("Bulk SMS with Twilio");
 })
 
-app.listen(port,function(error){
-    if(error){
+
+app.get("/sendsms", (req, res) => {
+    res.send("Hello world");
+    sendSMS();
+})
+
+app.listen(port, function (error) {
+    if (error) {
         console.log("Server fail");
     }
-    else{
+    else {
         console.log("Server Success");
     }
 })
