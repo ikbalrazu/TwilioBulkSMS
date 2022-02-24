cab89d909cc219e17b1b4da5cee9fdb715fcd41a
ikbalrazu <iqbalraju123@gmail.com>
2022-02-24 20:05:16 +0600

    collected the confiq data from frontend

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const cors = require('cors');
+const twilio = require('twilio');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
 
 const accountSid = 'ACadc3ffb43cef495abeafebce2609c137'; // Your Account SID from www.twilio.com/console
 const authToken = '1069b3764bb73164952c4effb0259a96'; // Your Auth Token from www.twilio.com/console
 
-const twilio = require('twilio');
-const client = new twilio(accountSid, authToken);
+
+//const client = new twilio(accountSid, authToken);
 
 function sendSMS() {
     console.log("send sms method");
@@ -26,6 +30,25 @@ function sendSMS() {
 
 app.get("/",(req,res)=>{
     res.send("Bulk SMS with Twilio");
+})
+
+app.post("/twilioconfiq",(req,res)=>{
+    const {phonenumber,twilionumber,twilioSID,twilioauthtoken,message} = req.body;
+    console.log(phonenumber);
+    console.log(twilionumber);
+    console.log(twilioSID);
+    console.log(twilioauthtoken);
+    console.log(message);
+    const client = new twilio('ACdd6486b54212f6bc7740b98a158177b2', 'a792e283777a09b5035176281fcfb3e0');
+    client.messages
+        .create({
+            body: message,
+            to: '+8801622869685', // Text this number
+            from: '+19034874646'
+        })
+        .then((message) => console.log(message.sid));
+    console.log("hello world");
+
 })
 
 
